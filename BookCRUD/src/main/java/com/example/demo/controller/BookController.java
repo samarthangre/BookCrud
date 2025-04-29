a633bf95908f633e1f198622b073e0500de7e5ab
samarthangre <sammyangre2002@gmail.com>
2025-04-29 13:57:44 +0530

    docker file updated

--- BookCRUD/src/main/java/com/example/demo/controller/BookController.java
+++ BookCRUD/src/main/java/com/example/demo/controller/BookController.java
@@ -3,8 +3,11 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +30,21 @@ public class BookController {
 	public List<Book>display(){
 		return bs.display();
 	}
+	
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable Integer id) {
+		bs.delete(id);
+	}
+	
+	@PutMapping("/update/{id}")
+	public void update(@RequestBody Book b, @PathVariable Integer id) {
+		b.setId(id);
+		bs.add(b);
+	}
+	
+//	@PostMapping("/search/{authorName}")
+//	public void search(@PathVariable String authorName) {
+//		
+//	}
+	
 }
